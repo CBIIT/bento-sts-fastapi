acbb32cce5997c25882de755f2ba52b44d561a39
knockknockyoo <iheaven0129@gmail.com>
2025-10-28 09:59:59 -0400

    removed unnecessary codes.

--- src/bento_sts/routers/terms.py
+++ src/bento_sts/routers/terms.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter, Depends, Request
 from ..dependencies import paging_params
-from ..pymodels import Term, CDE
-from ..converters import neo_to_py, neo_to_cde
 
 router = APIRouter(
     prefix="/terms",
@@ -168,59 +166,6 @@ def all_pvs_get(request: Request):
       CASE WHEN pv_val IS NOT NULL THEN collect({value: pv_val, synonyms: syn_vals, ncit_concept_code: ncit_oid}) ELSE [] END AS formatted_pvs
     RETURN cde.origin_id AS CDECode, cde.origin_version AS CDEVersion,
       cde.value AS CDEFullName, models, formatted_pvs AS permissibleValues"""
-
-    # stmt = """
-    # MATCH (cde:term)
-    #   WHERE toLower(cde.origin_name) CONTAINS "cadsr" and cde.origin_id='14596139'
-    # WITH cde
-    # MATCH (ent)-[:has_property]->(p:property)-[:has_concept]->(:concept)<-[:represents]-(cde)
-    #   WHERE p.model IS NOT NULL AND p.version IS NOT NULL
-    # OPTIONAL MATCH (p)-[:has_tag]->(use_null_tag:tag {key: "useNullCDE"})
-    # WITH cde,
-    #   COLLECT(DISTINCT {
-    #     model: p.model,
-    #     version: p.version,
-    #     property: ent.handle + "." + p.handle,
-    #     useNullCDE: COALESCE(use_null_tag.value, "No")
-    #   }) AS models,
-    #   ANY(ut IN COLLECT(use_null_tag) WHERE ut.value IN ["Yes", "True"] OR ut.value = true) AS should_use_null_cde
-    # WITH cde, models, should_use_null_cde,
-    #   cde.origin_id + "|" + COALESCE(cde.origin_version, "") AS cde_hdl
-    # // Get model pvs
-    # OPTIONAL MATCH (prop:property)-[:has_concept]->(c:concept)<-[:represents]-(cde)
-    # OPTIONAL MATCH (prop)-[:has_value_set]->(:value_set)-[:has_term]->(model_pv:term)
-    # WITH cde, models, cde_hdl, collect(DISTINCT model_pv) AS model_pvs, should_use_null_cde
-    # OPTIONAL MATCH (vs:value_set {handle: cde_hdl})-[:has_term]->(cde_pv:term)
-    # WITH cde, models, should_use_null_cde, model_pvs, COLLECT(DISTINCT cde_pv) AS cde_pvs
-    # // Get null CDE pvs if needed
-    # OPTIONAL MATCH (null_vs:value_set {handle: '16476366|1'})-[:has_term]->(null_pv:term)
-    # WHERE should_use_null_cde = true
-    # WITH cde, models, model_pvs, cde_pvs,
-    #   CASE WHEN should_use_null_cde THEN COLLECT(DISTINCT null_pv) ELSE [] END AS null_pvs
-    # WITH cde, models,
-    #   CASE
-    #     WHEN SIZE(cde_pvs) > 0 AND NONE(p IN cde_pvs WHERE p.value =~ "https?://.*")
-    #       THEN cde_pvs + null_pvs
-    #     WHEN SIZE(cde_pvs) > 0 AND ANY(p IN cde_pvs WHERE p.value =~ "https?://.*") AND SIZE(model_pvs) > 0
-    #       THEN model_pvs + null_pvs
-    #     WHEN SIZE(null_pvs) > 0
-    #       THEN null_pvs
-    #     ELSE [null]
-    #   END AS pvs
-    # WHERE size(pvs) > 0
-    # UNWIND pvs AS pv
-    # OPTIONAL MATCH (pv)-[:represents]->(c_cadsr:concept)<-[:represents]-(ncit_term:term {origin_name: "NCIt"}), (c_cadsr)-[:has_tag]->(:tag {key: "mapping_source", value: "caDSR"})
-    # OPTIONAL MATCH (ncit_term)-[:represents]->(c_ncim:concept)<-[:represents]-(syn:term), (c_ncim)-[:has_tag]->(:tag {key: "mapping_source", value: "NCIm"})
-    #   WHERE pv IS NOT NULL AND pv <> syn AND pv.value <> syn.value
-    # WITH cde, pv, models, pv.value as pv_val, ncit_term.origin_id AS ncit_oid,
-    #   ncit_term.value AS ncit_value,
-    #   collect(DISTINCT syn.value) AS distinct_syn_vals WITH cde, models, pv_val,
-    #   ncit_oid,
-    #   CASE WHEN ncit_value IS NOT NULL THEN distinct_syn_vals + [ncit_value] ELSE distinct_syn_vals END AS syn_vals
-    # WITH cde, models,
-    #   CASE WHEN pv_val IS NOT NULL THEN collect({value: pv_val, synonyms: syn_vals, ncit_concept_code: ncit_oid}) ELSE [] END AS formatted_pvs
-    # RETURN cde.origin_id AS CDECode, cde.origin_version AS CDEVersion,
-    #   cde.value AS CDEFullName, models, formatted_pvs AS permissibleValues"""
 
     stmt = " ". join([stmt,
                       f"SKIP {request.state.skip} " if request.state.skip else "",
